ad352e0dc2b9bedc103d237e91e3e5b0605e966c
Kaushik Mandal <koushik369mondal@gmail.com>
2026-07-06 14:29:51 +0530

    refactor object.js to improve employee object structure and logging

--- Day4/Evening/object.js
+++ Day4/Evening/object.js
@@ -3,6 +3,20 @@ let student = {
     age: 21,
     height: 5.6
 }
-console.log(student);
-console.log("Name: " + student.name);
-console.log("Name", student["name"]);
+// console.log(student);
+// console.log("Name: " + student.name);
+// console.log("Name", student["name"]);
+
+let employee = {
+    id: 101,
+    name: "Elon Musk",
+    salary: 50000,
+    address: {
+        city: "Kolkata",
+        state: "West Bengal",
+        country: "India"
+    }
+}
+console.log(employee);
+console.log("Name: " + employee.name);
+console.log("City: " + employee.address.city);
