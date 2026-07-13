737764fbf837f2e4210e784dbff8825b62e36425
Kaushik Mandal <koushik369mondal@gmail.com>
2026-07-13 11:53:26 +0530

    Add HTML and JavaScript files for SetInterval example

---
+++ Day7/setInterval/script.js
@@ -0,0 +1,10 @@
+let h = document.querySelector('h1');
+
+const change = setInterval(() => {
+    h.innerHTML = "hello" + Date.now();
+}, 4000)
+
+
+document.querySelector('button').addEventListener('click', () => {
+    clearInterval(change)
+})
