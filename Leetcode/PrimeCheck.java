0731aaad7705b28830c2646b5c81587ba1d583f4
Kaushik Mandal <koushik369mondal@gmail.com>
2026-07-14 20:24:49 +0530

    Add PrimeCheck program to determine if a number is prime

---
+++ Leetcode/PrimeCheck.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class PrimeCheck {
+
+    // Reusable function to check if a number is prime
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+
+        for (int i = 2; i <= num / 2; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+
+        if (isPrime(num)) {
+            System.out.println(num + " is a prime number.");
+        } else {
+            System.out.println(num + " is not a prime number.");
+        }
+
+        scanner.close();
+    }
+}
