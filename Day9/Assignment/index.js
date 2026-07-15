1b0cce35e439295dc471843169873e88c8b5f6c4
Kaushik Mandal <koushik369mondal@gmail.com>
2026-07-15 17:52:24 +0530

    Add file operations implementation and assignment notes

---
+++ Day9/Assignment/index.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+
+// WRITE
+fs.writeFileSync('temp.txt', 'Hello World');
+console.log('File Create');
+
+// READ
+// const data = fs.readFileSync('temp.txt', 'utf8');
+// console.log('File content: ', data);
+
+// UPDATE
+// fs.appendFileSync('temp.txt', '\nNew Data Added.');
+// console.log('File Updated.');
+
+// DELETE
+// fs.unlinkSync('temp.txt');
+// console.log('File Deleted');
+
+
