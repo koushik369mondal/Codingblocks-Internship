b309b34a947450369e0729f0d641bdc22ccd4578
Kaushik Mandal <koushik369mondal@gmail.com>
2026-07-16 11:30:34 +0530

    add endpoints to retrieve all users and specific user by ID

--- Day10/Lec12/app.js
+++ Day10/Lec12/app.js
@@ -6,8 +6,26 @@ const PORT = 8001;
 
 const users = require('./MOCK_DATA.json');
 
+// get all user data
 app.get('/users', (req, res) => {
     res.send(users)
+})
+
+app.get('/api/users', (req, res) => {
+    res.send(users)
+})
+
+// get particular data
+app.get('/api/users/:id', (req,res) => {
+    const id = req.params.id;
+
+    console.log(typeof(id))
+
+    const user = users.filter((item) => item.id == id);
+
+    console.log(user);
+
+    return res.json(user)
 })
 
 app.listen(PORT, () => {
