757353bb4698bbc1e1ce76db115b50835a5e9cea
Kaushik Mandal <koushik369mondal@gmail.com>
2026-07-16 10:46:30 +0530

    add query endpoint to log request parameters in app.js

--- Day10/app.js
+++ Day10/app.js
@@ -8,6 +8,12 @@ app.get('/', (req, res) => {
     res.send('hello students');
 });
 
+// query
+app.get('/name', (req, res) => {
+    // res.send('Hello Kaushik')
+    console.log(req.query);
+})
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`); // Note: The instructor is currently typing the console.log statement here
 });
